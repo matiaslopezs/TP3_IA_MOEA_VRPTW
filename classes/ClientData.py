d769304b880b0911313b5270d6c3c090033dc534
David <david.nunez@oktana.io>
2021-10-25 20:02:20 -0300

    Added ClientData class

--- classes/ClientData.py
+++ classes/ClientData.py
@@ -1,3 +1,4 @@
+import math
 class ClientData(object):
     def __init__(self, client_number, x, y, demand, ready_time, due_date, service_time):
         self.client_number = client_number
@@ -7,3 +8,17 @@ class ClientData(object):
         self.ready_time = ready_time
         self.due_date = due_date
         self.service_time = service_time
+        self.is_depot = self.client_number == 0
+
+    def get_distance_to_client(self, client):
+        client_a = [self.x, self.y]
+        client_b = [client.x, client.y]
+        return math.dist( client_a, client_b ) #Calculate the euclidean distance between points
+
+    def get_demand(self):
+        return self.demand
+
+    def timepoint_is_in_the_window(self, timepoint):
+        start = timepoint
+        end = timepoint + self.service_time
+        return start >= self.ready_time and self.due_date <= end
